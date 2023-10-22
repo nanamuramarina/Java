069701408a54b1f51efdb8a59193b3a00b01e279
nanamuramarina <nanamuramarina@gmail.com>
2023-10-22 14:01:45 +0000

    演習問題

--- CmdLine.java
+++ CmdLine.java
@@ -1,5 +1,7 @@
 class CmdLine{
   public static void main (String[] args) {
-    System.out.println("age");
+    System.out.println("ages[0] :" + args[0]);
+    System.out.println("ages[1] :" + args[1]);
+    System.out.println("ages.length" + args.length);
   }
 }
