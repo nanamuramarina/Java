a5176f5a38179a88eaeee89e69824bb6ad53ed6c
nanamuramarina <nanamuramarina@gmail.com>
2023-11-04 14:48:10 +0000

    課題途中

---
+++ Car1.java
@@ -0,0 +1,18 @@
+class Car1{
+  int no;
+  int speed;
+  
+  void setNo(int n){
+    no = n;
+  }
+  void run(int s) {
+    speed = s;
+  }
+  void break(){
+    speed = speed-s;
+  }
+  void display(){
+    System.out.println("ナンバー" + no + "の速度は" + speed + "です");
+  }
+  }
+}
