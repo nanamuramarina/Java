1cf94ebef428c7c2c4e9ddfd98c4b257e273eef2
nanamuramarina <nanamuramarina@gmail.com>
2023-11-11 03:41:38 +0000

    this,super

--- StuSample3.java
+++ StuSample3.java
@@ -1,10 +1,6 @@
-class StuSample3 {
-  public static void main(String[] args) {
-    Student stu1 = new Student("菅原");
-    Student stu2 = new Student("村山", 75, 100);
-
-    stu1.setScore(90, 80);
-    stu1.display();
-    stu2.display();
+public class StuSample3 {
+  public static void main (String[] args) {
+    Student3 stu = new Student3 ("菅原");
+    stu.display();
   }
 }
--- Student3.java
+++
@@ -1,8 +0,0 @@
-public class Student3 extends Person3 {
-  private int stu No;
-
-  public Student3(String name) {
-    this(name, 999);
-  }
-  public Student3(String name)
-}
