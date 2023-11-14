1acd5f1cdfd893cafe62372c466feec4eb8aa7ac
nanamuramarina <nanamuramarina@gmail.com>
2023-11-14 12:42:51 +0000

    プログラミング演習２

--- Car2.java
+++ Car2.java
@@ -1,23 +1,10 @@
-class Car2 {
-  int no;
-  int speed;
-  
-  Car2(){
-    no = 0;
+public class Car2 {
+  private int no;
+
+  public Car2 (int no) {
+    this.no = no;
   }
-  Car2(int n){
-    no = n;
-  }
-  void run(int s) {
-    speed = s;
-  }
-  void breake(){
-    speed = 0;
-  }
-  void breake(int s){
-    speed = speed - s;
-  }
-  void display(){
-    System.out.println("ナンバー" + no + "の速度は" + speed + "です");
+  public void display () {
+    System.out.println("ナンバーは" + no + "です");
   }
 }
