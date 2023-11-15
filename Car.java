c53cc4fdd12f2c772cf96238e97f3bc7ad76eaa7
nanamuramarina <nanamuramarina@gmail.com>
2023-11-15 13:29:56 +0000

    プログラミング演習３

--- Car.java
+++ Car.java
@@ -1,20 +1,8 @@
 class Car {
-  // メンバ変数
-  int no;
-  int speed;
-  
-  // メソッド
-  void setNo (int n) {
-    no = n;
+  Car() {
+    System.out.println("Car()");
   }
-  void run (int s) {
-    speed = s ;
-  }
-  
-  void stop () {
-    speed = 0;
-  }
-  void display () {
-    System.out.println("ナンバー" + no + "の速度は" + speed + "です");
+  Car(int no) {
+    System.out.println("Car(int no)");
   }
 }
